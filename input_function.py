dd80c3f397b78694e490a5830726bc34426fad7e
akibmahir <akibmahir6@gmail.com>
2025-10-08 17:28:41 +0600

    new commit

---
+++ input_function.py
@@ -0,0 +1,8 @@
+a = input("Enter the first no: ") # 32
+b = input("Enter the second no: ") # 45
+print(a + b) # output will be 3245 because in pyhton when a number is given as input, it will be taken as string 
+
+# for this we have to convert the input as int when we take numbers
+c = int(input("Enter the first number: ")) # 32
+d = int(input("Enter the second number: ")) # 45
+print(c + d) # output will 77

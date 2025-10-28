87bb3d736d847c66fe634868e2932e360df809a9
akibmahir <akibmahir6@gmail.com>
2025-10-29 04:33:19 +0600

    all

--- List/lists.py
+++ List/lists.py
@@ -13,3 +13,20 @@ l1.insert(3,8): This will add 8 at 3 index
 l1.pop(2): Will delete element at index 2 and return its value. 
 l1.remove(21): Will remove 21 from the list.
 """
+container2 = [2,3,1,7]
+'''
+In python, the sort(), reverse(), insert(), remove(), append() these functions returns "None" because these functions changes the original list. If i want to get the full list, at first write like below then print the original list.
+'''
+container2.sort()
+print(container2)
+
+container2.reverse()
+print(container2)
+
+container.insert(4, "Banana")
+print(container)
+
+print(container.pop(4))
+
+container.remove("Apple")
+print(container)
--- Tuple/tuple.py
+++ Tuple/tuple.py
@@ -1,6 +1,7 @@
 #tuple is same as lists but the difference is tuple is immutable
 a = (2, 3, 'Mahir', True, 'Apple', 32.4)
 b = (1,)
+c = ()
 """
 a = () # empty tuple 
 a = (1,) # tuple with only one element needs a comma 
@@ -11,3 +12,4 @@ a = (1, 7, 2)
 • a.count (1): a count (1) will return number of times 1 occurs in a. 
 • a.index (1) will return the index of first occurrence of 1 in a.
 """
+print(c) # output is ()

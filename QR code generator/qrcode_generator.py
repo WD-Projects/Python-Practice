d1ca827d77c0d4e8824bbaf3ac456c3f7ab914e4
akibmahir <akibmahir6@gmail.com>
2025-11-19 18:41:34 +0600

    added new

--- QR code generator/qrcode_generator.py
+++ QR code generator/qrcode_generator.py
@@ -1,7 +1,7 @@
 import qrcode
 def generate_qr():
     with open ("QR code generator/input.txt", "r") as file:
-        lines = file.readlines()
+        lines = file.readlines() #readlines() simply return an list like [https://www.youtube.com/?, youtube.png]
 
     store_image = qrcode.make(lines[0])
     store_image.save(lines[1])

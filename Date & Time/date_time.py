32495e06c559d5b25cb144d65de014f2cb389f0c
akibmahir <akibmahir6@gmail.com>
2025-11-28 16:54:37 +0600

    new all committed

---
+++ Date & Time/date_time.py
@@ -0,0 +1,19 @@
+from datetime import datetime, date, time, timedelta
+now = datetime.now()
+print(now)
+print(now.year)
+print(now.month)
+print(now.day)
+print(now.date())
+print(datetime.now().time())
+print(now.strftime("%B %d, %Y"))
+
+ekhon = date.today()
+print(ekhon)
+
+ekhon = date(year = 1999, month = 4, day = 12)
+print(ekhon)
+print(ekhon.year)
+
+ekhon = datetime.now()
+print(ekhon + timedelta(days = 5, minutes = 5))
